a9887f42ff4445a96f6e45b18c043dba07f9fdbe
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2024-02-07 09:10:23 -0300

    se agrego site

---
+++ src/site/SiteController.java
@@ -0,0 +1,21 @@
+package career;
+
+import common.Controller;
+
+/**
+ * Controller for site management.
+ * This controller implements the Controller interface.
+ * It provides methods to manage site information.
+ *
+ * @author Giuliano Ignacio Poeta
+ */
+public class SiteController implements Controller {
+
+    /**
+     * Main method of the site controller.
+     * This method is the main entry point for site management.
+     */
+    public void main() {
+        // Implementation of the main method of the site controller
+    }
+}
