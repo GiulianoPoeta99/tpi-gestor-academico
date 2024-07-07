9a002fb98ef35865bb1a3daba337d7241a4efb92
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2024-07-07 15:29:52 -0300

    se agregaron javadocs a site

--- src/main/site/SiteViews.java
+++ src/main/site/SiteViews.java
@@ -10,8 +10,28 @@ import main.common.components.LayOut;
 import main.common.components.Text;
 import main.common.components.UIComponent;
 
+/**
+ * The SiteViews class provides methods to manage the layout and components
+ * of the main site view. It includes methods to set up the main layout with
+ * redirections and to generate components for the index view.
+ *
+ * <p>Methods:</p>
+ * <ul>
+ *   <li>layout(Map&lt;String, Runnable&gt; redirections) - Sets up the main site layout.</li>
+ *   <li>index() - Generates components for the index view.</li>
+ * </ul>
+ *
+ * @version 1.0.0
+ * @author Giuliano Ignacio Poeta
+ * @since 2024.02.07
+ */
 public class SiteViews {
 
+    /**
+     * Sets up the main site layout with the specified redirections.
+     *
+     * @param redirections A map of redirection actions for the side panel.
+     */
     public static void layout(Map<String, Runnable> redirections) {
         JFrame frame = LayOut.mainFrame();
         JPanel topPanel = LayOut.topPanel();
@@ -26,6 +46,11 @@ public class SiteViews {
         frame.setVisible(true);
     }
 
+    /**
+     * Generates and returns a list of components for the index view.
+     *
+     * @return A list of JComponents for the index view.
+     */
     public static List<JComponent> index() {
         List<JComponent> components = new ArrayList<>();
 
